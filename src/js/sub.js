184f56c9998641dbb795db3549fdc6919a98d7e2
Tsuyoshi-I <tsuyoshifl244@gmail.com>
2019-06-01 08:51:36 +0900

    fitst commit

---
+++ src/js/sub.js
@@ -0,0 +1,3 @@
+import '../scss/lib.scss'
+import '../scss/reset.scss'
+import '../scss/sub.scss'
---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+
+const MODE = "development";
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const fileName = MODE == 'production' ? '[name]-[hash]' : '[name]';
+
+module.exports = {
+  mode: MODE,
+  entry: {
+    main: "./src/js/main.js",
+    sub: "./src/js/sub.js"
+  },
+  output: {
+    filename: `${fileName}.js`,
+    path: __dirname + "/dist"
+  },
+  devServer: {
+    contentBase: "./dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: [
+                require("autoprefixer")({
+                  grid: true
+                })
+              ]
+            }
+          },
+          'sass-loader',
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            attrs: ['img:src', ':data-src']
+          }
+        }
+      },
+      {
+        test: /\.(gif|png|jpg|jpeg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 51200,
+              name: `./img/${fileName}.[ext]`,
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  { 'modules': false },
+                ]
+              ]
+            }
+          }
+        ],
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks: ['main'],
+      filename: "main.html",
+      template: 'src/main.html'
+    }),
+    new HtmlWebpackPlugin({
+      chunks: ['sub'],
+      filename: "sub.html",
+      template: 'src/sub.html'
+    }),
+    new CleanWebpackPlugin(),
+  ],
+}
