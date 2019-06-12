a454ddf7b5fdc612da1db006ac9837eda5d15c68
森　泰樹 <taiki@mbp-2.local>
2019-06-12 10:52:43 +0900

    contact form spサイトのレイアウト完成

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,1 +1,8 @@
-import "../scss/contact.scss"
+// import flatpickr from "flatpickr";
+// // import 'flatpickr/dist/flatpickr.css'; // formで使用するカレンダーアドオン
+// import "../scss/contact.scss"
+
+// flatpickr('#t', {
+//   enableTime: true,
+//   dateFormat: "Y-m-d H:i",
+// });
--- src/js/sub.js
+++ src/js/sub.js
@@ -1,1 +1,2 @@
+import './contact';
 import '../scss/contact.scss';
