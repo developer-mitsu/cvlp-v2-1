40c302535dc9598c1c34cbe3d7d6c8bd3be43df2
森　泰樹 <taiki@mbp-2.local>
2019-06-15 18:19:17 +0900

    header footerをテンプレート化しました

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,13 @@
-
 const MODE = "development";
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const fileName = MODE == 'production' ? '[name]-[hash]' : '[name]';
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const fileName = MODE == "production" ? "[name]-[hash]" : "[name]";
 
 module.exports = {
   mode: MODE,
   entry: {
     main: "./src/js/main.js",
-    contact: "./src/js/sub.js",
+    contact: "./src/js/sub.js"
   },
   output: {
     filename: `${fileName}.js`,
@@ -22,9 +21,9 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          'style-loader',
+          "style-loader",
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               importLoaders: 2
             }
@@ -39,69 +38,77 @@ module.exports = {
               ]
             }
           },
-          'sass-loader',
+          "sass-loader"
         ]
       },
       {
-        test: /\.html$/,
-        use: {
-          loader: 'html-loader',
-          options: {
-            attrs: ['img:src', ':data-src']
+        test: /\.ejs$/,
+        use: [
+          {
+            loader: "extract-loader",
+            options: {
+              publicPath: "./dist"
+            }
+          },
+          {
+            loader: "html-loader",
+            options: {
+              attrs: ["img:src", ":data-src"]
+            }
+          },
+          {
+            loader: "ejs-compiled-loader"
           }
-        }
+        ]
       },
       {
         test: /\.(gif|png|jpg|jpeg)$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 51200,
-              name: `./img/${fileName}.[ext]`,
+              name: `./img/${fileName}.[ext]`
             }
           }
         ]
       },
       {
         test: /\.php$/,
-        use: [{
-            loader: 'file-loader',
+        use: [
+          {
+            loader: "file-loader",
             options: {
-                name: '[name].[ext]',
+              name: "[name].[ext]"
             }
-        }]
+          }
+        ]
       },
       {
         test: /\.js$/,
         use: [
           {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: [
-                [
-                  '@babel/preset-env',
-                  { 'modules': false },
-                ]
-              ]
+              presets: [["@babel/preset-env", { modules: false }]]
             }
           }
         ],
-        exclude: /node_modules/,
+        exclude: /node_modules/
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      chunks: ['main'],
+      // chunks: ["main"],
       filename: "main.html",
-      template: 'src/main.html'
+      template: "src/main.ejs"
     }),
     new HtmlWebpackPlugin({
-      chunks: ['contact'],
+      // chunks: ["contact"],
       filename: "contact.html",
-      template: 'src/contact.html'
+      template: "src/contact.ejs"
     }),
-    new CleanWebpackPlugin(),
-  ],
-}
+    new CleanWebpackPlugin()
+  ]
+};
